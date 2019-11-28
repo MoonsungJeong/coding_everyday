298fe6a23b9e3f93a47dcd18aab70fa3de221d4f
Moonsung <gmsj159@gmail.com>
2019-11-28 10:57:59 +1000

    C code algorithm / 2019-11-288

---
+++ 063.c
@@ -0,0 +1,14 @@
+// 3-54 구의 지름을 입력받고, 구의 표면적(겉넓이)을 계산하여 출력하세요.
+// 구의 표면적 = 4*반지름*반지름*파이 (단, 파이는 = 3.141592)
+#include<stdio.h>
+#define PI 3.141592
+int main(){
+	double r,result;
+	printf("표면적을 구할 구의 지름을 입력하세요: ");
+	scanf("%lf",&r);
+	result = 4*(r/2)*(r/2)*PI;
+	printf("지름 %.3f인 구의 표면적 = %.3f\n",r,result); 
+
+	return 0;
+}
+
